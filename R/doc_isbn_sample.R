509c01cc83ff8357bce234a0ef652076667e4805
Yohann Mansiaux <yohann@thinkr.fr>
2024-02-02 13:28:43 +0100

    change isbn_sample data to keep BD only

--- R/doc_isbn_sample.R
+++ R/doc_isbn_sample.R
@@ -2,10 +2,8 @@
 #'
 #' Description.
 #'
-#' @format A data frame with 100000 rows and 2 variables:
+#' @format A data frame with  rows and  variables:
 #' \describe{
-#'   \item{ code }{  integer64 }
-#'   \item{ gtinType }{  character }
 #' }
-#' @source Source
+#' @source https://nudger.fr/opendata
 "isbn_sample"
--- data-raw/isbn_sample.R
+++ data-raw/isbn_sample.R
@@ -5,8 +5,15 @@ library(data.table)
 library(magrittr)
 
 #  Fichier supprimé car gigantesque
-big_db <- fread("data-raw/open4goods-full-gtin-dataset.csv", nrows = 5000000, select = c(1, 7))
-isbn_sample <- big_db[gtinType == "ISBN_13"][sample(.N, 1e5)]
+big_db <- fread("data-raw/open4goods-full-gtin-dataset.csv", nrows = 5000000)
+isbn_sample <- big_db[gtinType == "ISBN_13"]
+isbn_sample <- rbind(
+  isbn_sample[grepl(pattern = "BD", x = isbn_sample$categories)],
+  isbn_sample[grepl(pattern = "BANDE DESSINEE", x = isbn_sample$categories)]
+) |>
+  unique()
+
+isbn_sample <- isbn_sample[["code"]]
 
 usethis::use_data(isbn_sample, overwrite = TRUE)
 checkhelper::use_data_doc("isbn_sample")
