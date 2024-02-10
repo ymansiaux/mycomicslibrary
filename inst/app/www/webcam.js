6cf7ff6194be8a2bd08a7bef9a88cf9e02e667ae
Yohann Mansiaux <yohann@thinkr.fr>
2024-02-10 13:54:27 +0100

    Camera can be shutdown

--- inst/app/www/webcam.js
+++ inst/app/www/webcam.js
@@ -4,11 +4,23 @@ $(document).ready(function () {
     let camera_button = document.querySelector("#webcam-start-camera");
     let video = document.querySelector("#webcam-video");
     let click_button = document.querySelector("#webcam-click-photo");
+    let stop_camera = document.querySelector("#webcam-stop-camera");
     let canvas = document.querySelector("#webcam-canvas");
 
     camera_button.addEventListener('click', async function () {
         let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
         video.srcObject = stream;
+    });
+
+    stop_camera.addEventListener('click', async function () {
+        let stream = video.srcObject;
+        let tracks = stream.getTracks();
+        // Merci Copilot :p 
+        tracks.forEach(function (track) {
+            track.stop();
+        });
+        video.srcObject = null;
+        video.ariaHidden = true;
     });
 
     click_button.addEventListener('click', function () {
@@ -17,6 +29,5 @@ $(document).ready(function () {
         let image_data_url = canvas.toDataURL('image/jpeg');
         Shiny.setInputValue('base64url', image_data_url, { priority: 'event' });
         canvas.style.display = "none";
-        // data url of the image
     });
 });
