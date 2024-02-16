7f8b30b00a92f5020458b9076ca22d9353add30d
Yohann Mansiaux <yohann@thinkr.fr>
2024-02-16 10:20:11 +0100

    wip : Adding a button works

--- inst/app/www/gridjs.js
+++ inst/app/www/gridjs.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
   Shiny.addCustomMessageHandler('build_grid', function (arg) {
     new gridjs.Grid({
-      columns: arg.columns,
+      columns: [
+        "Species",
+        {
+          name: 'ht',
+          formatter: (cell) => gridjs.html(`${cell}`)
+        }
+      ],
       search: true,
       data: arg.data
+      // data: Array(5).fill().map(x => [
+      //   "12",
+      //   "<h1> coucou </h1>"
+      // ])
     }).render(document.getElementById(arg.id));
   })
 });
