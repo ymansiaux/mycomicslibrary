3b0e676c09d6dac0879b40d35e85c22f315ebc65
Yohann Mansiaux <yohann@thinkr.fr>
2024-02-16 14:43:14 +0100

    Un peu de CSS dans gridjs

--- R/mod_130_poc_gridjs.R
+++ R/mod_130_poc_gridjs.R
@@ -11,37 +11,6 @@ mod_130_poc_gridjs_ui <- function(id) {
   ns <- NS(id)
   tagList(
     tags$div(
-      # selectInput(
-      #   inputId = ns("select"),
-      #   label = "Select",
-      #   choices = c(
-      #     NULL
-      #   )
-      # )
-      selectizeInput(
-        inputId = ns("select"),
-        label = "Select",
-        choices = c(
-          "0" = make_stars_rating_div(note = 0),
-          "1" = make_stars_rating_div(note = 1),
-          "2" = make_stars_rating_div(note = 2),
-          "3" = make_stars_rating_div(note = 3)
-        ),
-        options = list(
-          render = I(
-            '{
-        item: function(item, escape) {
-          return "<div>" + item.value + "</div>";
-          },
-        option: function(item, escape) {
-          return "<div>" + item.value + "</div>";
-          }
-        }'
-          )
-        )
-      )
-    ),
-    tags$div(
       class = "col",
       h3("Top Ten Homme"),
       tags$div(id = ns("top_ten_homme"))
@@ -56,42 +25,16 @@ mod_130_poc_gridjs_server <- function(id, r_global) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    # updateSelectizeInput(
-    #   session,
-    #   inputId = "select",
-    #   choices = c(
-    #     "0" = make_stars_rating_div(note = 0),
-    #     "1" = make_stars_rating_div(note = 1),
-    #     "2" = make_stars_rating_div(note = 2),
-    #     "3" = make_stars_rating_div(note = 3)
-    #   ),
-    #   options = list(
-    #     render = I(
-    #       '{
-    #     item: function(item, escape) {
-    #       return "<div>" + item.value + "</div>";
-    #       },
-    #     option: function(item, escape) {
-    #       return "<div>" + item.value + "</div>";
-    #       }
-    #     }'
-    #     )
-    #   ),
-    #   server = FALSE
-    # )
-
-    observeEvent(input$select, {
-      print(input$select)
-    })
 
     observeEvent(r_global$comics_db, {
       # browser()
 
-      db <- prepare_comics_db_to_see_collection(
-        r_global$comics_db,
-        ns = ns
-      )
-
+      db <- r_global$comics_db |>
+        get_most_recent_entry_per_doc() |>
+        prepare_comics_db_to_see_collection(
+          ns = ns
+        )
+      # il faut garder la dernière entrée
       # browser()
 
       golem::invoke_js(
--- inst/app/www/gridjs.js
+++ inst/app/www/gridjs.js
@@ -62,6 +62,20 @@ $(document).ready(function () {
       pagination: {
         limit: 5,
         summary: false
+      },
+      style: {
+        table: {
+          border: '3px solid #ccc'
+        },
+        th: {
+          'background-color': 'rgba(0, 0, 0, 0.1)',
+          color: '#000',
+          'border-bottom': '3px solid #ccc',
+          'text-align': 'center'
+        },
+        td: {
+          'text-align': 'center'
+        }
       }
     }).render(document.getElementById(arg.id));
   })
